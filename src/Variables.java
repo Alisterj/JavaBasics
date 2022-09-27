0a0e4f189482e26d6b09e4c55d09b744344aec41
Alisterj <lisarsiss@gmail.com>
2022-09-27 17:22:39 +0300

    Two lesson - variables

---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 557;
+        short myShort = 3266;  // Only 16bit numbers (-32000 to 32000)
+        long myLong = 3255456; // Bigger data
+
+        double myDouble = 255.33; // It has 64 bits information
+        float myFloat = 2356.65f;
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 100;  // It has 8 bit or 1 byte information (-128 - 128)
+
+        System.out.println(myInt);
+    }
+}
