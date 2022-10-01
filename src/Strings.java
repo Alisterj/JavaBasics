0e4fd64e413ee693aec71df81cc1cd03efd73c52
Alisterj <lisarsiss@gmail.com>
2022-10-01 19:10:12 +0400

    Strings

---
+++ src/Strings.java
@@ -0,0 +1,14 @@
+/*
+* Ссылочные типы данных
+* */
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "Alisterj";
+        //объединение строк
+        String space = " ";
+        String name = "Alister";
+        System.out.println(s + space + name);
+        System.out.println(x + " " + name);
+    }
+}
