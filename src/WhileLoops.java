9cc2a92e62e00cd6a078c32f59db899906893cdd
Alisterj <lisarsiss@gmail.com>
2022-10-01 19:21:47 +0400

    While

---
+++ src/WhileLoops.java
@@ -0,0 +1,23 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+        //boolean t = true;
+        //boolean t = 5>3;
+        //boolean t = 5>=5;
+        //boolean t = 5>5;
+        //boolean t = 5>7;
+        boolean t = 5 == 5;
+        System.out.println(t);
+
+        ////////////////////////////////////
+        int value = 0;
+        boolean tr = value > 5;
+        System.out.println(tr);
+
+        ///////////////////////////////////
+        int valueTwo = 0;
+        while (valueTwo < 5){
+            System.out.println("Hello " + valueTwo);
+            valueTwo++;
+        }
+    }
+}
