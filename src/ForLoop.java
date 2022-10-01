9212c0a9e560a625792e6f5126a1af6f0fc4d4e8
Alisterj <lisarsiss@gmail.com>
2022-10-01 19:30:58 +0400

    For

---
+++ src/ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i = 0; i<=10; i++)
+            System.out.println("Alisterj " + i);
+        for (int i = 10; i >= 0; i -= 5)
+            System.out.println("Archy " + i);
+    }
+}
