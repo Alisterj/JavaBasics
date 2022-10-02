946f4cfb99fb7456413e0e631e0ffeb4aef7bfa8
Alisterj <lisarsiss@gmail.com>
2022-10-02 13:46:35 +0400

    If

---
+++ src/If.java
@@ -0,0 +1,11 @@
+public class If {
+    public static void main(String[] args) {
+        int myInt = 19;
+        if (myInt<10)
+            System.out.println(myInt + " is less 10");
+        else if (myInt>20)
+            System.out.println(myInt + " is more 20");
+        else
+            System.out.println("Ни один из случаев не сработал");
+    }
+}
