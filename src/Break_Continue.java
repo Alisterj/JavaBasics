5aee148cadd2afbf899b141e73aa95a245724b5d
Alisterj <lisarsiss@gmail.com>
2022-10-05 12:31:23 +0400

    Break and Continue

---
+++ src/Break_Continue.java
@@ -0,0 +1,20 @@
+public class Break_Continue {
+    public static void main(String[] args) {
+        int i = 0;
+        while (true){
+            if (i == 15)
+                break;
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Конец цилка");
+
+        ////////////////////////////////////////
+
+        for (int t = 0; t <= 15; t++){
+            if (t % 2 == 0)
+                continue;
+            System.out.println(t);
+        }
+    }
+}
