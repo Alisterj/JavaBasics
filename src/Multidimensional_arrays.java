b88afdd39b7bf42f5412f9706ea4c8424afdbe89
Alisterj <lisarsiss@gmail.com>
2022-10-06 16:16:35 +0400

    Multidimensional Arrays

---
+++ src/Multidimensional_arrays.java
@@ -0,0 +1,31 @@
+public class Multidimensional_arrays {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+        System.out.println(numbers[1]);
+
+        int[][] matrice = {
+                            {1,2,3,4,5},
+                            {6},
+                            {7,8, 9}};
+        System.out.println(matrice[1][0]);
+        System.out.println(matrice[2][2]);
+        System.out.println(matrice[0][4]);
+
+        ////////////////////////////////
+
+        int[] numberOne = new int[5];
+
+        String[][] strings = new String[2][3];
+        strings[0][1] = "Alister";
+        System.out.println(strings[0][1]);
+
+
+        /////////////////////////////////////
+        for (int i = 0; i < matrice.length; i++){
+            for (int j = 0; j < matrice[i].length; j++)
+                System.out.print(matrice[i][j] + " ");
+            System.out.println();
+        }
+
+    }
+}
