e1e7f68191a1e9cc910259eb45e3be78ee6f7605
Alisterj <lisarsiss@gmail.com>
2022-10-24 17:27:55 +0300

    fomatting strings

---
+++ src/Lesson23Continued.java
@@ -0,0 +1,22 @@
+public class Lesson23Continued {
+    public static void main(String[] args) {
+        System.out.print("Alisterj");
+        System.out.println("Alisterj again");
+        System.out.printf("This is a string, %s%n","ARCHIE");
+        //or
+        System.out.printf("This is a number, %d%n",125);
+
+        //Помимо обычной работы вставкой, можно и форматировать
+
+        System.out.printf("String %10d \n", 566);
+        System.out.printf("String %-10d \n", 5);
+        System.out.printf("String %10d \n", 56600000);
+        System.out.printf("String %10d \n", 566000000);
+        //для вещественных чисел
+        //при работе с числами может округлять их
+        System.out.printf("String %.2f \n", 32.9);
+        System.out.printf("String %.2f \n", 32.917545);
+
+        // % [10][-6][.11]
+    }
+}
