b08ac74a82c2042c8e6999b48cef53d70052039c
Alisterj <lisarsiss@gmail.com>
2022-10-25 16:39:40 +0300

    Object

---
+++ src/Lesson24ClassObjectAndToString.java
@@ -0,0 +1,22 @@
+public class Lesson24ClassObjectAndToString {
+    public static void main(String[] args) {
+        String str = "Alisterj";
+        System.out.println(str);
+        //создание объекта + попытка произвести печать
+        HumanFour24 h = new HumanFour24("Alisterj", 24);
+        System.out.println(h);
+        //System.out.println(h.toString()); верхнее тоже самое
+    }
+}
+class HumanFour24 {
+    private String name;
+    private int age;
+
+    public HumanFour24(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String toString(){
+        return name + ", " + age;
+    }
+}
