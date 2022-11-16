a51c5c6a476619c22671ecb4bb59570556cceee0
Alisterj <lisarsiss@gmail.com>
2022-11-16 12:29:45 +0300

    Some about primitive data types

---
+++ src/Lesson32/Test.java
@@ -0,0 +1,19 @@
+package Lesson32;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(x);
+
+        Integer x2 = 123;
+        String str1 = new String("222");
+        //Пометочка
+        //Классы Integer,  String ... по умолчанию встроены в java
+        //И лежат в пакете java.lang
+        //И как известно доступны по умолчанию - без импорта
+        Integer.parseInt("123");
+
+        Boolean.
+
+    }
+}
